3979226f09f5141917dc18ede6e48967710666d9
PedroTrudes <pedro.jtrudes@gmail.com>
2026-03-02 11:59:11 -0300

    feat: criacao de exceptions para retorno de api / subindo Postgres no docker / Criando modelo CandidateEntity

---
+++ src/main/java/br/com/pedrotrudes/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,30 @@
+package br.com.pedrotrudes.gestao_vagas.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [usarname] não deve conter espaco")
+    private String username;
+
+    @Email(message = "O campo [email] deve ter um email valido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre 10 a 100 caracteres.")
+    private String password;
+
+    private String description;
+    private String curriculum;
+
+
+}
---
+++ src/main/java/br/com/pedrotrudes/gestao_vagas/modules/candidate/controller/CandidateCotroller.java
@@ -0,0 +1,19 @@
+package br.com.pedrotrudes.gestao_vagas.modules.candidate.controller;
+
+import br.com.pedrotrudes.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateCotroller {
+
+    @PostMapping("/")
+    public void create( @Valid @RequestBody CandidateEntity candidateEntity){
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }
+}
